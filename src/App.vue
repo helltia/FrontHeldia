6f6eafb5abfbaf917e120ad93b3575f8c9027d33
Luis Daniel Aldana de la Rosa <ldaldana001@gmail.com>
2023-11-11 18:18:58 -0600

    Solved image route error that prevented build

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 
     <NavBar class="navbar navbar-expand-lg navbar-light fixed-top" />
     <router-view />
-    
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </template>
 
 <script>
-import NavBar from "@/components/NavBar.vue";
+import NavBar from "./components/Navbar";
 
 export default {
   components: {
